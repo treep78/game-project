cf732f7e14e9b3be1df8ce7f79b68f29f296fb7b
treep78 <treep78@gmail.com>
2016-11-01 11:17:09 -0400

    improve log in and out menus

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -13,6 +13,9 @@ const signUpSuccess = function(data)
   store.token = data.user.token;
   $('#messages').text('New user signed up!');
   console.log(data);
+  $('#sign-up-modal').modal('hide');
+  $('#sign-in-button').html("<form id='sign-out'><input type='submit' name='submit' value='Sign Out!'></form>");
+  $('#sign-up-button').html('<button type="button" class="btn btn-primary btn-lg" data-toggle="modal"data-target="#change-password-modal">Change Password</button>');
 };
 
 const signInSuccess = function(data)
@@ -20,18 +23,25 @@ const signInSuccess = function(data)
   store.user = data.user;
   console.log(data);
   $('#messages').text('User signed in!');
+  $('#sign-in-modal').modal('hide');
+  console.log($('#sign-in-button').text());
+  $('#sign-in-button').html("<form id='sign-out'><input type='submit' name='submit' value='Sign Out!'></form>");
+  $('#sign-up-button').html('<button type="button" class="btn btn-primary btn-lg" data-toggle="modal"data-target="#change-password-modal">Change Password</button>');
 };
 
 const changePasswordSuccess = function(data)
 {
   console.log(data);
   $('#messages').text('Password Changed!');
+  $('#change-password-modal').modal('hide');
 };
 
 const signOutSuccess = function()
 {
   store.user = null;
   $('#messages').text('User signed out!');
+  $('#sign-in-button').html('<button type="button" class="btn btn-primary btn-lg" data-toggle="modal"data-target="#sign-in-modal">Sign In</button>');
+  $('#sign-up-button').html('<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#sign-up-modal">Sign Up</button>');
 }
 
 const failure = (error) => {
