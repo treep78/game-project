1e3481d55013a344d38a0c236da865ebbed9d399
treep78 <treep78@gmail.com>
2016-11-03 22:56:30 -0400

    build for deployment

--- bundle.js
+++ bundle.js
@@ -420,7 +420,8 @@ webpackJsonp([0],[
 	  var games = [0, 0]; //played, won
 	  console.log(data);
 	  store.sessionID = 0;
-	  for (var i in data.games) {
+	  var i = void 0;
+	  for (i in data.games) {
 	    games[0] += 1;
 	    if (store.user.email === data.games[i].player_x.email && data.games[i].id > store.sessionID) {
 	      store.sessionID = data.games[i].id;
